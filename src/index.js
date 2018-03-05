558342de73c04676e51de2f0ca71ef8d7c47f92f
Voskobovich-Alex <voskobovich555@gmail.com>
2018-03-06 02:04:34 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+var k = 0;
+var l = 2;
+var nok = new Array();
+	do {
+ 		if (base % l == 0) {
+  			nok[k] = l;
+  			k++;
+  			base = base / l;
+ 		} else {
+  			l++;
+	 }
+	}
+		while (l < base);
+			nok[k] = l; 
+
+var counter = 0;
+  for (var i = l; number/i >= 1; i *= l){
+  	 counter += Math.trunc(number/i);
+
+  }
+	
+  return counter;
+
+
+};
